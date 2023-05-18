6c0c6ea784a695ce0525d948deb9c0c43c698e17
Your Name <Rimon0000>
2023-05-18 21:47:49 +0600

    registration done

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCEXGQeanqKk-mEIhEI7-2F3WJqp8-AL58",
+  authDomain: "toy-client-808e8.firebaseapp.com",
+  projectId: "toy-client-808e8",
+  storageBucket: "toy-client-808e8.appspot.com",
+  messagingSenderId: "881299773491",
+  appId: "1:881299773491:web:d1743ae95a503ae11eeb46"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app 
