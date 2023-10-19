3b299982f0e2ba2a92b51cb4d341a040e1cecde1
Your Name <Rimon0000>
2023-10-19 12:18:37 +0600

    up to date

--- src/Pages/Home/About/About.jsx
+++ src/Pages/Home/About/About.jsx
@@ -7,7 +7,8 @@ const About = () => {
     return (
         <div>
             <div className='mx-auto text-center px-10 mt-11'>
-                <h2 className='text-center text-4xl font-bold pt-5 px-5 pb-2'>
+                <h2 className='text-center text-4xl font-bold pt-5 px-5 pb-8
+                '>
                     About Us
                 </h2>
                 <div className='lg:flex md:flex gap-10'>
